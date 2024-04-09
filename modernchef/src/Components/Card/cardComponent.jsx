72898bb7bdc36205eb6376fd391d92ff1373b9b7
PedroZotelli <pzotelli2@gmail.com>
2024-04-08 23:37:56 -0300

    Components updates

--- modernchef/src/Components/Card/cardComponent.jsx
+++ modernchef/src/Components/Card/cardComponent.jsx
@@ -1,42 +1,44 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import './cardComponent.css';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./cardComponent.css";
 
 function Cards() {
   return (
     <div className="container">
       <div className="card-wrapper">
-        <div className="card mb-3" style={{ maxWidth: '540px' }}>
+        <div className="card mb-3" style={{ maxWidth: "600px", maxHeight: "auto" }}>
           <div className="row g-0">
             <div className="col-md-4">
-              <Card.Img variant="top" src="holder.js/100px180" className="img-fluid rounded-start" alt="Card image" />
+              <Card.Img
+                variant="top"
+                src="holder.js/100px180"
+                className="img-fluid rounded-start"
+                alt="Card image"
+              />
             </div>
             <div className="col-md-8">
               <div className="card-body">
-                <Card.Title>To com Fome e Sono(nome da comida)</Card.Title>
-                <Card.Text>
-                  Salmón, Papaya, Tofu, Alcachofas, Miel,
-                  Queso azul, Quinoa, Remolacha, Jengibre, Aceitunas, 
-                  Espárragos, Salsa de soja, Cúrcuma, Semillas de chía, Pimientos rojos, Fresas, Menta, Arroz integral, Aguacate, Vinagre balsámico
-                </Card.Text>
-                <Button variant="primary">Veja os Benefícios</Button>
+                <Card.Title>Feijoada</Card.Title>
+                <Card.Text>Feijuca, bão demais!</Card.Text>
+                <Card.Text>Serve 2 pessoass</Card.Text>
+                <Button variant="primary">Ver Receita e Nutrientes</Button>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div className="card-wrapper">
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>To com Fome e Sono(nome da comida)</Card.Title>
-            <Card.Text>
-              Salmón, Papaya, Tofu, Alcachofas, Miel,
-              Queso azul, Quinoa, Remolacha, Jengibre, Aceitunas, 
-              Espárragos, Salsa de soja, Cúrcuma, Semillas de chía, Pimientos rojos, Fresas, Menta, Arroz integral, Aguacate, Vinagre balsámico
-            </Card.Text>
-            <Button variant="primary">Abrir receita completa</Button>
-          </Card.Body>
+        <Card bg="dark" text="white">
+          <Card.Img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXwVKqwBm52U-J1qi4ZLhCZV5h-HCDNzVTPDiXX5FucQ&s"
+            className="card-img"
+            alt="Card image"
+          />
+          <div className="card-overlay">
+            <Card.Title>Fruta do Dia</Card.Title>
+            <Card.Text>Laranja Lima</Card.Text>
+            <Button variant="primary">Ver Benefícios</Button>
+          </div>
         </Card>
       </div>
     </div>
